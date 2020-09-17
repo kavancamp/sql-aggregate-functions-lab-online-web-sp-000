4f1c4893e826fc33ada13a71b13bfbe5197d7192
Keenah Vancampenhout <kvancamp@mail.com>
2020-09-17 14:52:16 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -4,11 +4,11 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
-  SELECT MIN(gpa) FROM students;
+  "SELECT MIN(gpa) FROM students;"
 end
 
 def average_student_gpa
-  SELECT AVG(gpa) AS average_student_gpa FROM students;
+  "SELECT AVG(gpa) AS average_student_gpa FROM students;"
 end
 
 def total_tardies_for_all_students
